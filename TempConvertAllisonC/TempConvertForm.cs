8a4827c6ae7895666328f6ca14e8630603cce63d
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-04-13 12:59:36 -0400

    Initial commit

---
+++ TempConvertAllisonC/TempConvertForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 13 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Temperature Converter
+ * This program converts the temp from celsuis to fahrenheit
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConvertAllisonC
+{
+    public partial class frmTempConvert : Form
+    {
+        public frmTempConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
